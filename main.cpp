09aa37577b4f9656d9dbf49f5a66fd011fd545f8
Adilet <adiletbaimyrza@gmail.com>
2023-05-10 19:09:50 +0200

    added gitignore file

--- main.cpp
+++ main.cpp
@@ -5,23 +5,14 @@
 
 std::string text
 {
-    "Sure! In Rabin-Karp algorithm, we use hash function to calculate the hash value of the pattern,"
-    "which is used to compare with the hash values of substrings of the given text. If a substring's hash"
-    "value matches with the pattern's hash value, then we compare the substring and pattern character by character to"
-    "check if they are actually equal.\n"
-    "\n"
-    "A simple hash function that can be used for Rabin-Karp algorithm is to treat the characters of the pattern as digits"
-    "in a base-radix number system, where radix.....A is the number of distinct characters in the input alphabet. For example, if the input alphabet"
-    "is {a, b, c, d, e}, then the radix is 5. Then, we can use a polynomial hash function to calculate the hash value of the pattern:\n"
-    "\n"
-    "hash(pattern) = (p[0]*radix^(m-1) + p[1]*radix^(m-2) + ... + p[m-1]*radix^0) mod q\n"
-    "\n"
-    "where p[i] is the numerical value of the i-th character in the pattern (i.e., its position in the input alphabet), m is"
-    "the length of the pattern, radix is the base..... of the adie number system (i.e., the size of the input alphabet), and q is a large prime"
-    "number that is used as the modulus to avoid overflow."
+    "Colors bounced around in her head."
+    "They mixed and threaded themselves together."
+    "Even colors that had no business being together."
+    "They were all one, yet distinctly separate at the same time."
+    "How was she going to explain this to the others?"
 };
 
-std::string pattern{".."};
+std::string pattern{"the"};
 
 int main() {
     std::cout << rabin_karp::search(pattern, text).size() << std::endl;
